79d96d00e3ddf7d6bf09bef323226fe14c498992
slavavoinov <voijnov@yandex.ru>
2022-05-17 17:52:50 +0300

    4.1.2. Глупый бот [#31323 #301663]

--- src/main/java/ru/job4j/condition/DummyBot.java
+++ src/main/java/ru/job4j/condition/DummyBot.java
@@ -11,7 +11,7 @@ public class DummyBot {
         return rsl;
     }
 
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         String rsl = DummyBot.answer("Привет, Бот.");
         System.out.println(rsl);
         rsl = DummyBot.answer("Пока.");
