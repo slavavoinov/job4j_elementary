5656456ee0282834517542c283fa3e94d20d07f5
slavavoinov <voijnov@yandex.ru>
2022-05-25 17:43:27 +0300

    5.5. Простое число [#171694]

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -15,11 +15,4 @@ public class CheckPrimeNumber {
         }
         return prime;
     }
-
-    public static void main(String[] args) {
-        System.out.println(CheckPrimeNumber.check(5));
-        System.out.println(CheckPrimeNumber.check(4));
-        System.out.println(CheckPrimeNumber.check(1));
-    }
-
 }
--- src/test/java/ru/job4j/loop/CheckPrimeNumberTest.java
+++ src/test/java/ru/job4j/loop/CheckPrimeNumberTest.java
@@ -1,5 +1,4 @@
 package ru.job4j.loop;
-
 import org.junit.Assert;
 import org.junit.Test;
 
