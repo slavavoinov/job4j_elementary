89f3efe9974ea150a8f8aad972e82ecc8f467176
slavavoinov <voijnov@yandex.ru>
2022-11-09 10:51:01 +0300

    6.9. String - это массив [#504884 #358342]

--- src/main/java/ru/job4j/array/JavaNameValidator.java
+++ src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -7,15 +7,15 @@ public class JavaNameValidator {
     public static boolean isNameValid(String name) {
         if (name.isEmpty() || isUpperCase(name.codePointAt(0)) || isDigit(name.codePointAt(0))) {
             return false;
-        } else {
+        }
             for (int i = 1; i < name.length(); i++) {
-                if (!(isSpecialSymbol(name.codePointAt(i)) || isUpperLatinLetter(name.codePointAt(i)) || isLowerLatinLetter(name.codePointAt(i)) || isDigit(name.codePointAt(i)))) {
+                if (!(isSpecialSymbol(name.codePointAt(i)) || isUpperLatinLetter(name.codePointAt(i))
+                        || isLowerLatinLetter(name.codePointAt(i)) || isDigit(name.codePointAt(i)))) {
                     return false;
                 }
             }
             return true;
         }
-    }
 
     public static boolean isSpecialSymbol(int code) {
         return code == 36 || code == 95;
